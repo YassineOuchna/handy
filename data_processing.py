503456ff3d9d57265f90abff15a6447d095b60fd
Yassine Ouchna <yassine.ouchna@student-cs.fr>
2023-10-06 22:10:00 +0200

    saving the final dataset

--- data_processing.py
+++ data_processing.py
@@ -4,6 +4,7 @@ import json as j
 import numpy as np
 from tensorflow import keras
 
+" Running this file visualizes the images, labels and first layer of a CNN and saves the final dataset"
 
 """---  IMPORTING RAW TRAINING DATA  ---"""
 
@@ -13,7 +14,9 @@ training_data_directory = "./data/training/rgb"
 batch_size = 64
 image_size = [128, 128]
 
-# Turning raw images into a keras tensor : 130240 images of shape 128 x 128 x 3 (rgb values)
+# Turning raw images into a keras tensor 
+# 130240 images of shape 128 x 128 x 3 (rgb values) 
+# that are put into batches of size 64
 images_raw = keras.utils.image_dataset_from_directory(
     training_data_directory,
     label_mode=None,
@@ -32,7 +35,7 @@ with open("./data/training_xyz.json", "r") as f:
 
 with open("./data/training_K.json", "r") as k:
     K = j.load(k)
-    K = tf.convert_to_tensor(K)  # 3D Coordonates of camera position
+    K = tf.convert_to_tensor(K) # 3D Coordonates of camera position
 
 
 def visualize_img(images_raw):  # Shows 6 images of the dataset
@@ -54,22 +57,29 @@ uv = tf.transpose(
     tf.linalg.matmul(K, tf.transpose(xyz_file, perm=(0, 2, 1))), perm=(0, 2, 1)
 )
 label_raw = uv[:, :, :2] / uv[:, :, -1:]
-label_raw = label_raw * (127 / 224)  # resizing the coordinates to a 128 x 128 shape
+# resizing the coordinates to a 128 x 128 shape
+label_raw = label_raw * (127 / 224)  
 
-# Augmented label tensor
+# for each label, we have 4 corresponding images 
+# having the same hand posture in different conditions
+
 L1 = tf.concat([label_raw, label_raw], 0)
 L2 = tf.concat([label_raw, label_raw], 0)
-label_tensor = tf.concat([L1, L2], 0)
-label_tensor = label_tensor
 
-# Creating the label dataset
+
+
+label_tensor = tf.concat([L1, L2], 0) # tensor of shape 130240 x 21 x 2 
+                                      # having the (x,y) of 21 hand posture defining points
+
+
+# Creating a dataset with both images and labels with batches of size 64
 label_origin = tf.data.Dataset.from_tensor_slices(label_tensor).batch(
     batch_size, drop_remainder=True
 )
 dataset = tf.data.Dataset.zip((images_raw, label_origin))
 
 
-def visualize_img_labels():  # shows the first 6 images with their corresponding labels
+def visualize_img_labels():       # shows the first 6 images with their corresponding labels
     plt.figure(figsize=(10, 10))
     for element in images_raw:
         for s in range(6):
@@ -77,9 +87,7 @@ def visualize_img_labels():  # shows the first 6 images with their corresponding
             for i in range(5):
                 X = [label_raw[s][k + i * 4][0] for k in range(1, 5)]
                 Y = [label_raw[s][k + i * 4][1] for k in range(1, 5)]
-                X.insert(
-                    0, label_raw[s][0][0]
-                )  # inserting the coordinates of the root point 0
+                X.insert(0, label_raw[s][0][0])  # inserting the coordinates of the root point 0
                 Y.insert(0, label_raw[s][0][1])
                 ax.plot(X, Y)
                 plt.imshow(element[s])
@@ -89,18 +97,17 @@ def visualize_img_labels():  # shows the first 6 images with their corresponding
 
 """---  CREATING A LABEL LAYER  ---"""
 
-
+# from a set of points (raw label), creates an image of the label
+# of the form 128 x 128 filled with ones and zeros 
 def create_label(label):
-    label = tf.cast(
-        label, dtype=tf.int64
-    )  # changing the type from float to int
-    label = tf.clip_by_value(label, 0, 127)  # On écrete les valeurs extrèmes
+    label = tf.cast(label, dtype=tf.int64)                  # changing the type from float to int
+    label = tf.clip_by_value(label, 0, 127)                 # removing points outside the 128 range
     batch_lvl = tf.concat(
         [tf.ones(21, dtype=tf.int64) * i for i in range(batch_size)], axis=0
     )
     point_lvl = tf.concat([tf.range(21, dtype=tf.int64)] * batch_size, axis=0)
-    coordinates2 = tf.cast(label[:, :, 0], dtype=tf.int64)  # On récupère les X
-    coordinates1 = tf.cast(label[:, :, 1], dtype=tf.int64)  # On récupère les Y
+    coordinates2 = tf.cast(label[:, :, 0], dtype=tf.int64)  # loading x-axis values
+    coordinates1 = tf.cast(label[:, :, 1], dtype=tf.int64)  # loading y-axis values
     indices = tf.stack(
         [
             batch_lvl,
@@ -117,15 +124,13 @@ def create_label(label):
     )
     x = tf.sparse.reorder(
         sparse_label
-    )  # on réordonne les dimension pour le bon fonctionnement de l'opération
+    )                                                          # Reordering dimensions
     not_sparse = tf.sparse.to_dense(x, validate_indices=False)
     return not_sparse
 
-
+# Applies a gaussian blur on one image of a label
+# returning an tensor image of shape 128 x 128 x 21
 def __gaussian_blur(img, kernel_size=11, sigma=3):
-    """
-    Applies a gaussian filter to one image
-    """
 
     def gauss_kernel(channels, kernel_size, sigma):
         ax = tf.range(-kernel_size // 2 + 1.0, kernel_size // 2 + 1.0)
@@ -146,28 +151,35 @@ def __gaussian_blur(img, kernel_size=11, sigma=3):
 
 
 def create_layer(label):
-    parsed_label = create_label(label)
-    return __gaussian_blur(parsed_label)
+    parsed_label = create_label(label)   # label image
+    return __gaussian_blur(parsed_label) # gauss'ed label image i.e layer
 
 
 def load_dataset():
-    img_dataset = images_raw
-    label_dataset = label_origin.map(create_layer)
-    return tf.data.Dataset.zip((img_dataset, label_dataset))
+    layer_tensor = label_origin.map(create_layer)           
+    return tf.data.Dataset.zip((images_raw, layer_tensor))   # Returns an image x layer dataset
 
+# Shows 3 images with their corresponding heatmap of key points
 def visualize_layer(data):
-    plt.figure(figsize=(10, 6))
-    for element in data: 
+    plt.figure(figsize=(4, 6))
+    for element in data:            # element being the particular batch 
         for i in range(3):
             ax = plt.subplot(2, 3, i + 1)
             image_label=tf.reduce_sum(element[1][i], axis=-1)
             plt.imshow(image_label)
-            ax=plt.subplot(2,3,i+2)
+            ax=plt.subplot(2,3,i+4)
             plt.imshow(element[0][i])
             plt.axis("off")
         plt.show()
         break
 
+
+"""---  SAVING THE FINAL DATASET  ---"""
+
+path='./data/dataset'
+load_dataset().save(path)
+
 if __name__ == "__main__":
+    visualize_img_labels()
     visualize_layer(load_dataset())
     print("executed as main")
