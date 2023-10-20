57009345bf3c446a523e4853c0d7de7f5987e4ab
Yassine Ouchna <yassine.ouchna@student-cs.fr>
2023-10-20 18:17:49 +0200

    building the model

--- cnn_model.py
+++ cnn_model.py
@@ -2,20 +2,40 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import layers
 
-"--- CONVOLUTION BLOCK ---"
+
+"--- CONVOLUTION Block ---"
+
+# We define the initial number of neurons
+N = 16
+
 
 class ConvBlock(tf.keras.Model):
     def __init__(self, out_dim):
         super(ConvBlock, self).__init__()
-        self.core=keras.Sequential(
-            layers.SpatialDropout2D(0.3),
-            layers.Conv2D(out_dim, kernel_size=3,padding="same", activation="relu"),
-            layers.BatchNormalization(),
-            layers.SpatialDropout2D(0.3),
-            layers.Conv2D(out_dim, kernel_size=3,padding="same", activation="relu"),
-            layers.BatchNormalization(),
-            layers.SpatialDropout2D(0.3),
-            layers.Conv2D(out_dim, kernel_size=3,padding="same", activation="relu"),
-        )
-    
-    
+        self.core = keras.Sequential()
+        self.core.add(layers.SpatialDropout2D(0.3))
+        self.core.add(
+            layers.Conv2D(out_dim, kernel_size=3,
+                          padding="same", activation="relu"))
+        self.core.add(
+            layers.Conv2D(out_dim, kernel_size=3,
+                          padding="same", activation="relu"))
+        self.core.add(
+            layers.SpatialDropout2D(0.3))
+        self.core.add(
+            layers.BatchNormalization())
+        self.core.add(
+            layers.SpatialDropout2D(0.3))
+        self.core.add(
+            layers.Conv2D(out_dim, kernel_size=3,
+                          padding="same", activation="relu"))
+
+
+'''---  BUILDING THE MODEL --- '''
+
+model = ConvBlock(N)
+for i in range(3):
+    model.core.add(ConvBlock(N*(2)**(i+1)))
+
+model.build((128, 128, 3))
+model.summary()
