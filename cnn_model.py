8f89b8de565c17bc43edb8cad01309193b0b6280
Yassine Ouchna <yassine.ouchna@student-cs.fr>
2023-10-20 23:41:47 +0200

    skip connections

--- cnn_model.py
+++ cnn_model.py
@@ -9,6 +9,22 @@ from keras import layers
 N = 16
 
 
+def convblock(x, out_dim):
+    # l0=layers.BatchNormalization()(x)
+    # l1 = layers.SpatialDropout2D(0.3)(l0)
+    # l2= layers.Conv2D(out_dim, kernel_size=3,
+    #                     padding="same", activation="relu")(l1)
+    l3 = layers.BatchNormalization()(x)
+    l4 = layers.SpatialDropout2D(0.3)(l3)
+    l5 = layers.Conv2D(out_dim, kernel_size=3,
+                       padding="same", activation="relu")(l4)
+    l6 = layers.BatchNormalization()(l5)
+    l7 = layers.SpatialDropout2D(0.3)(l6)
+    y = layers.Conv2D(out_dim, kernel_size=3,
+                      padding="same", activation="relu")(l7)
+    return y
+
+
 class ConvBlock(tf.keras.Model):
     def __init__(self, out_dim):
         super(ConvBlock, self).__init__()
@@ -33,9 +49,8 @@ class ConvBlock(tf.keras.Model):
 
 '''---  BUILDING THE MODEL --- '''
 
-model = ConvBlock(N)
+# Input node
+x = layers.Input(shape=(128, 128, 3))
+model = convblock(x, N)
 for i in range(3):
     model.core.add(ConvBlock(N*(2)**(i+1)))
-
-model.build((128, 128, 3))
-model.summary()
